6e032d01fff991174e1cd4338d04dbe4b2b162a9
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-11 16:25:22 -0300

    back to home

--- src/app/sign-up.tsx
+++ src/app/sign-up.tsx
@@ -1,14 +1,20 @@
+import { Link } from 'expo-router'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Criar conta</Text>
+      <Link href="/" style={styles.back} >Voltar</Link>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  title: { fontSize: 22, fontWeight: 'bold' },
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center' ,
+    backgroundColor: '#cecece'
+  },
+  back: { fontSize: 16, fontWeight: 'bold' },
 })
