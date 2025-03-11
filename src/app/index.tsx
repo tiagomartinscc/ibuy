2f4b5eeaed53138319def132641ac66c43481ad1
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-11 16:30:01 -0300

    button as a link

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'expo-router'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Link href="/sign-up" style={styles.new}>
-        Criar conta
+      <Link href="/sign-up" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.label}>Criar conta</Text>
+        </TouchableOpacity>
       </Link>
     </View>
   )
@@ -13,5 +15,11 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  new: { fontSize: 16, fontWeight: 'bold' },
+  label: { fontSize: 16, fontWeight: 'bold', color: '#fff'},
+  button: {
+    backgroundColor: '#000',
+    paddingVertical: 10,
+    paddingHorizontal: 32,
+    borderRadius: 10
+  }
 })
