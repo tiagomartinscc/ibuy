880f0709dcea8d39d564739530888d5793caae93
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-11 16:52:10 -0300

    config stack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,6 @@
-import { Slot } from "expo-router"
-import { View } from "react-native"
+import { Stack } from "expo-router"
 export default function Layout() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ width: '100%', height: 70, backgroundColor: '#000' }} />
-      <Slot />
-      <View style={{ width: '100%', height: 50, backgroundColor: '#C3C3C3' }} />
-    </View>
+    <Stack screenOptions={{ headerShown: false }} />
   )
 }
