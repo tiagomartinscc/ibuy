42e7a2876aca1580c54a923b87ae684bf10f04d9
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-12 07:32:33 -0300

    update expo and change to tabs navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,6 @@
-import { Stack } from "expo-router"
+import { Tabs } from "expo-router"
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }} />
+    <Tabs screenOptions={{ headerShown: false }} />
   )
 }
