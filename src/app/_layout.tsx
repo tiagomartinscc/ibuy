0334c8d0a5e7baae6f4a3e8fcdb57ee673920c11
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-12 07:47:40 -0300

    Drawer navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,26 +1,10 @@
-import { Tabs } from "expo-router"
-import { MaterialIcons } from '@expo/vector-icons'
+import { Drawer } from 'expo-router/drawer'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function Layout() {
   return (
-    <Tabs screenOptions={{ 
-      headerShown: false, 
-      tabBarLabelPosition: 'beside-icon',
-      tabBarInactiveTintColor: '#cecece',
-      tabBarActiveTintColor: '#000'
-    }}>
-      <Tabs.Screen 
-        name="index" 
-        options={{
-          tabBarLabel: 'Sign In',
-          tabBarIcon: ({color}) => <MaterialIcons name="home" size={20} color={color} />
-      }} />
-      <Tabs.Screen 
-        name="sign-up" 
-        options={{
-          tabBarLabel: 'Sign Up',
-          tabBarIcon: ({color}) => <MaterialIcons name="person" size={20} color={color} />
-      }} />
-    </Tabs>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer />
+    </GestureHandlerRootView>    
   )
 }
