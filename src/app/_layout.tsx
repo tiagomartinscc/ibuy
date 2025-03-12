95810c258e18d7452d7e92945c876d60d48096be
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-12 07:41:59 -0300

    custom tabs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,26 @@
 import { Tabs } from "expo-router"
+import { MaterialIcons } from '@expo/vector-icons'
+
 export default function Layout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }} />
+    <Tabs screenOptions={{ 
+      headerShown: false, 
+      tabBarLabelPosition: 'beside-icon',
+      tabBarInactiveTintColor: '#cecece',
+      tabBarActiveTintColor: '#000'
+    }}>
+      <Tabs.Screen 
+        name="index" 
+        options={{
+          tabBarLabel: 'Sign In',
+          tabBarIcon: ({color}) => <MaterialIcons name="home" size={20} color={color} />
+      }} />
+      <Tabs.Screen 
+        name="sign-up" 
+        options={{
+          tabBarLabel: 'Sign Up',
+          tabBarIcon: ({color}) => <MaterialIcons name="person" size={20} color={color} />
+      }} />
+    </Tabs>
   )
 }
